96fb0d35a8dd31af76813f1caf30d551dbecdbc3
Tim van Osch <timvosch@pollex.nl>
2020-09-29 21:54:43 +0200

    SPI test

---
+++ boards/mfm-micro/include/board.h
@@ -0,0 +1,35 @@
+#if !defined(_BOARD_H_)
+#define _BOARD_H_
+
+#include <stm32l0xx_hal.h>
+#include <stm32l0xx_hal_gpio.h>
+
+/*
+  Pin definitions
+*/
+
+#define LED0_Pin GPIO_PIN_3
+#define LED0_Port GPIOB
+
+/*
+  Peripheral SPI definitions
+*/
+
+#define PERIPHERAL_SPI_AF GPIO_AF0_SPI1
+#define PERIPHERAL_SPI_Instance SPI1
+#define PERIPHERAL_SPI_Port GPIOA
+
+#define PERIPHERAL_SCK_Pin GPIO_PIN_5
+#define PERIPHERAL_MISO_Pin GPIO_PIN_6
+#define PERIPHERAL_MOSI_Pin GPIO_PIN_7
+
+#define PERIPHERAL_INT_Pin GPIO_PIN_13
+#define PERIPHERAL_INT_Port GPIOB
+
+#define PERIPHERAL1_NSS_Pin GPIO_PIN_0
+#define PERIPHERAL1_NSS_Port GPIOB
+
+#define PERIPHERAL2_NSS_Pin GPIO_PIN_1
+#define PERIPHERAL2_NSS_Port GPIOB
+
+#endif
--- include/main.h
+++ include/main.h
@@ -2,8 +2,6 @@
 #define _MAIN_H_
 
 #include <stm32l0xx_hal.h>
-
-#define LED0_Pin GPIO_PIN_3
-#define LED0_GPIO_Port GPIOB
+#include "board.h"
 
 #endif
